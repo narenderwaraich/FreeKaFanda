34eb91217c6db8bfdd7aef6e966dedd081cec3cd
singhWaraich <singh4narender@gmail.com>
2020-03-07 18:49:06 +0530

    change home page route

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 |
 */
 Route::get('/myip', 'TestController@getIP');
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 
